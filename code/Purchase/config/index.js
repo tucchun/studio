53c16a7f28b1c1156ca48dec1706586e7c38af09
tucchun <>
2018-05-08 19:24:26 +0800

    upload

---
+++ code/Purchase/config/index.js
@@ -0,0 +1,125 @@
+/* eslint key-spacing:0 spaced-comment:0 */
+import path from 'path'
+import _debug from 'debug'
+import { argv } from 'yargs'
+import ip from 'ip'
+
+const localip = ip.address()
+const debug = _debug('app:config')
+debug('Creating default configuration.')
+
+// ========================================================
+// Default Configuration
+// ========================================================
+const config = {
+  env : process.env.NODE_ENV || 'development',
+  RUN_IN: (process.env.RUN_IN && process.env.RUN_IN.toLowerCase()) || 'local',
+
+  // ----------------------------------
+  // Project Structure
+  // ----------------------------------
+  path_base  : path.resolve(__dirname, '..'),
+  dir_client : 'src',
+  dir_dist   : 'dist',
+  dir_server : 'server',
+  dir_test   : 'tests',
+
+  // ----------------------------------
+  // Server Configuration
+  // ----------------------------------
+  server_host : localip, // use string 'localhost' to prevent exposure on local network
+  server_port : process.env.PORT || 3000,
+
+  // ----------------------------------
+  // Compiler Configuration
+  // ----------------------------------
+  compiler_css_modules     : false,     // disable css modules
+  compiler_devtool         : 'source-map',
+  compiler_hash_type       : 'hash',
+  compiler_fail_on_warning : false,
+  compiler_quiet           : false,
+  compiler_public_path     : '/',
+  compiler_stats           : {
+    chunks : false,
+    chunkModules : false,
+    colors : true
+  },
+  compiler_vendor : [
+    'preact',
+    'preact-router',
+    'preact-compat'
+  ]
+}
+
+/************************************************
+-------------------------------------------------
+
+All Internal Configuration Below
+Edit at Your Own Risk
+
+-------------------------------------------------
+************************************************/
+
+// ------------------------------------
+// Environment
+// ------------------------------------
+// N.B.: globals added here must _also_ be added to .eslintrc
+config.globals = {
+  'process.env'  : {
+    'RUN_IN' : JSON.stringify(config.RUN_IN)
+  },
+  'NODE_ENV'     : config.env,
+  '__DEV__'      : config.env === 'development',
+  '__PROD__'     : config.env === 'production',
+  '__TEST__'     : config.env === 'test',
+  '__DEBUG__'    : config.env === 'development' && !argv.no_debug,
+  '__BASENAME__' : JSON.stringify(process.env.BASENAME || ''),
+  '__RUN_IN_DEV__': config.RUN_IN.toLowerCase() === 'dev',
+  '__RUN_IN_FVT__': config.RUN_IN.toLowerCase() === 'fvt',
+  '__RUN_IN_UAT__': config.RUN_IN.toLowerCase() === 'uat',
+  '__RUN_IN_PRD__': config.RUN_IN.toLowerCase() === 'prd'
+}
+
+// ------------------------------------
+// Validate Vendor Dependencies
+// ------------------------------------
+const pkg = require('../package.json')
+
+config.compiler_vendor = config.compiler_vendor
+  .filter((dep) => {
+    if (pkg.dependencies[dep]) return true
+
+    debug(
+      `Package "${dep}" was not found as an npm dependency in package.json; ` +
+      `it won't be included in the webpack vendor bundle.
+       Consider removing it from vendor_dependencies in ~/config/index.js`
+    )
+  })
+
+// ------------------------------------
+// Utilities
+// ------------------------------------
+const resolve = path.resolve
+const base = (...args) =>
+  Reflect.apply(resolve, null, [config.path_base, ...args])
+
+config.utils_paths = {
+  base   : base,
+  client : base.bind(null, config.dir_client),
+  dist   : base.bind(null, config.dir_dist)
+}
+
+// ========================================================
+// Environment Configuration
+// ========================================================
+debug(`Looking for environment overrides for RUN_IN "${config.RUN_IN}".`)
+const environments = require('./environments').default
+const overrides = environments[config.RUN_IN]
+if (overrides) {
+  debug('Found overrides, applying to default configuration.')
+  Object.assign(config, overrides(config))
+} else {
+  debug('No environment overrides found, defaults will be used.')
+}
+
+export default config
